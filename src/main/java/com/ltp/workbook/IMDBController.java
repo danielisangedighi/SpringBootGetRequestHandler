c08eb15ed3b34f4dc49155d4219b12c3e60a45c8
danielisangedighi <danielisangedighi@outlook.com>
2024-05-05 20:05:00 +0100

    Model-View-Controller implementation

--- src/main/java/com/ltp/workbook/IMDBController.java
+++ src/main/java/com/ltp/workbook/IMDBController.java
@@ -3,6 +3,7 @@ package com.ltp.workbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 
 @Controller
@@ -10,7 +11,10 @@ public class IMDBController {
     @GetMapping("/shows")
     //public String getMethodName(@RequestParam String param) {
         //return new String();
-    public String getShows() {
+    public String getShows(Model model) {
+        Show show = new Show("Reacher","Welcome to Margrave", 7.1);
+        //Show show = new Show("Breaking Bad", "Ozymandias", 10.0);
+        model.addAttribute("Show", show);
         return "shows";
     }
     
